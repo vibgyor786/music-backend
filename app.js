1e4b52c5b89caf20b0886c33c388607930e602c2
ganesh <ganiiscool@gmail.com>
2023-07-11 14:58:18 +0530

    access control

--- app.js
+++ app.js
@@ -3,16 +3,14 @@ const app = express();
 require("dotenv/config");
 const cors = require("cors");
 const { default: mongoose } = require("mongoose");
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-})
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// })
 
-// app.use(cors({
-//   origin: 'http://localhost:3000/'
-// }));
+app.use(cors());
 
 // user authentication routes
 const userRoute = require("./routes/auth");
