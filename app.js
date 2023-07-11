0e17f2f789a22f87f0f516ca04d53ccee3121dd2
ganesh <ganiiscool@gmail.com>
2023-07-11 14:10:38 +0530

    access control

--- app.js
+++ app.js
@@ -6,24 +6,18 @@ const { default: mongoose } = require("mongoose");
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://music-frontend-ix4o.vercel.app/');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+ res.setHeader('Access-Control-Allow-Credentials', true);
 
   // Pass to next layer of middleware
   next();
 });
 let corsOptions = {
   // origin: "https://code2clone.ieeessitvit.tech",
-  origin:"https://music-frontend-ix4o.vercel.app/",
+  origin:"http://localhost:3000",
   credentials: true,
 
   methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
