c0e6c31ecc82df5697b7c463eff3759bcc28eb1f
ganesh <ganiiscool@gmail.com>
2023-07-11 15:40:07 +0530

    cors

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ require("dotenv/config");
 const cors = require("cors");
 const { default: mongoose } = require("mongoose");
 // app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT,DELETE");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   // res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT,DELETE");
+//   // res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   res.setHeader("HTTP/1.1 200 OK")
 //   next();
 // })
 let corsOptions = {
